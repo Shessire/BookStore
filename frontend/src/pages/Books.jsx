d567cf4e614f7be23175ff46510c90e866d1e574
Wisuwat <wisuwatpond01@gmail.com>
2023-08-28 17:56:15 +0700

    fix img

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -35,7 +35,7 @@ const Books = () => {
         <div className="books">
             {books.map((book) => (
                 <div key={book.id} className='book' >
-                    {book.cover && <img src={book.cover} alt="" />}
+                    <img src={book.cover} alt="" />
                     <h2>{book.title}</h2>
                     <p>{book.desc}</p>
                     <button className="delete" onClick={()=>handleDelete(book.id)}>Delete</button>
